4cbfd265b234cb47a17658f1f1f080d658e94939
jacob <358819135@qq.com>
2022-09-27 22:59:09 +0800

    code init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less">
+.home {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
